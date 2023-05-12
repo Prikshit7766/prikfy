ec1a48b43d177f459bfcd593fbc8f332f54411b2
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-05-12 11:25:32 +0530

    custom_exception

--- src/prikfy/custom_exception.py
+++ src/prikfy/custom_exception.py
@@ -1,16 +1,11 @@
 import sys
 
-
-def error_message_detail(error):
-    exc_type, exc_value, exc_tb = sys.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    error_message = f"Error occurred in Python script: {file_name}, line number: {line_number}, error message: {str(error)}"
-    return error_message
-
 class CustomException(Exception):
-    def __init__(self, error_message):
-        self.error_message = error_message_detail(error_message)
-    
+    def __init__(self, message):
+        self.message = message
+        _, _, tb = sys.exc_info()
+        self.file_name = tb.tb_frame.f_code.co_filename
+        self.line_number = tb.tb_lineno
+
     def __str__(self):
-        return self.error_message
+        return f"Error occurred in {self.file_name} at line {self.line_number}: {self.message}"
