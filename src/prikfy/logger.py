442e0a92ced1f48a3b9e0ea8bb991e7f715c6b34
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-06-04 01:31:03 +0530

    logger

--- src/prikfy/logger.py
+++ src/prikfy/logger.py
@@ -4,15 +4,15 @@ import logging
 
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-log_dir = "./logs"
-log_filepath = os.path.join(log_dir, "runnig_logs.log")
-os.makedirs(log_dir, exist_ok=True)
+# log_dir = "./logs"
+# log_filepath = os.path.join(log_dir, "runnig_logs.log")
+# os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
     handlers=[
-        logging.FileHandler(log_filepath),  # this will print logs in file
+        # logging.FileHandler(log_filepath),  # this will print logs in file
         logging.StreamHandler(sys.stdout),
     ],  # this will print logs on console as well as in file
 )
